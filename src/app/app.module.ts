db36f37a03a59a9b7d4353a441bae31e814d9af3
Ostroski <allan_s11@hotmail.com>
2024-08-04 13:15:16 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 
 @NgModule({
@@ -27,6 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ButtonModule,
     FormsModule,
     CheckboxModule,
+    InputTextModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
