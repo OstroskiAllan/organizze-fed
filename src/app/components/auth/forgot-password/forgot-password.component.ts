d6e2bfc6926ed9a854266cdde0bd200ec40413b2
Ostroski <allan_s11@hotmail.com>
2025-01-02 16:41:23 -0300

    Correcoes nas telas de login e alteracoes

--- src/app/components/auth/forgot-password/forgot-password.component.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['../auth.scss']
 })
 export class ForgotPasswordComponent {
+  forgotForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.forgotForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 }
